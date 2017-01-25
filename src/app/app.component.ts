e6757159552f0bd789d1cffa6149c8da1df6968b
malikasinger1 <inzamam1>
2017-01-25 20:36:10 +0500

    things operated from component, in this case fired an action on button click result reflected on ui becase ui is directly binded with redux state which is present in store module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { NgRedux, select } from 'ng2-redux';
+
+import { IAppState, CounterAction } from './store';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+
+    //listening to store member 'select'
+    @select() counter$: Observable<number>;
+
+    constructor(
+        private counterAction: CounterAction,
+        private ngRedux: NgRedux<IAppState>
+    ) {
+
+        this.counter$.subscribe(val => {
+            console.log(val);
+        })
+    }
+
+    ngOnInit() {
+    }
+
+    increment(): void {
+        this.counterAction.increment();
+    }
+
+    decrement(): void {
+        this.counterAction.decrement();
+    }
+
 }
